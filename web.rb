628939be8057d186e6f86287bcd6c2a24a515c2e
Oren Kanner <okanner@gmail.com>
2014-12-14 11:52:20 -0500

    it lives :-)

--- web.rb
+++ web.rb
@@ -2,10 +2,22 @@ require 'sinatra'
 require 'slim'
 require 'pry'
 
-require 'hue'
+require 'lifx'
 require 'stock_quote'
+
+load './price_light.rb'
+include PriceLight
 
 get '/' do
   @title = 'Home'
   slim :index, :locals => { title: @title }
+end
+
+get '/quote/:symbol/:start_date/:end_date' do
+  @title = "#{params[:symbol]} Quote"
+  data = generate_data_arrays(params[:symbol], params[:start_date], params[:end_date])
+  client = LIFX::Client.lan
+  client.discover! { |c| c.lights.count == 3 }
+  process_scenes(client.lights.with_label('one'), data)
+  slim :quote, :locals => { title: @title, symbol: params[:symbol] }
 end
