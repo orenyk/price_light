8edb5abe126904c30bb95345563c4e484f39370d
Oren Kanner <okanner@gmail.com>
2014-12-14 11:58:15 -0500

    added multiple bulb routes

--- web.rb
+++ web.rb
@@ -13,11 +13,11 @@ get '/' do
   slim :index, :locals => { title: @title }
 end
 
-get '/quote/:symbol/:start_date/:end_date' do
+get '/:light/quote/:symbol/:start_date/:end_date' do
   @title = "#{params[:symbol]} Quote"
   data = generate_data_arrays(params[:symbol], params[:start_date], params[:end_date])
   client = LIFX::Client.lan
   client.discover! { |c| c.lights.count == 3 }
-  process_scenes(client.lights.with_label('one'), data)
+  process_scenes(client.lights.with_label(params[:light]), data)
   slim :quote, :locals => { title: @title, symbol: params[:symbol] }
 end
